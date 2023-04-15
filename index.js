6eeead5b360549d2087bc5049678767338380f2f
Akash Panday <akashpanday11@gmail.com>
2023-04-15 14:31:18 +0530

    added rate limiter

--- index.js
+++ index.js
@@ -5,18 +5,24 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const AuthRoutes = require("./routes/auth");
 const movieRoutes = require("./routes/movies");
+const rateLimit = require("express-rate-limit");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 dotenv.config();
 
+const limiter = rateLimit({
+  windowMs: 1000, // 1 second
+  max: 1, // limit each IP to 1 requests per windowMs
+});
+
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
-
+app.use(limiter);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
