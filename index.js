5a2144b497e6b50220840590aa5e5d0d2bf370a8
Akash Panday <akashpanday11@gmail.com>
2023-04-23 19:24:05 +0530

    tryinh

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       "https://cinebook.netlify.app",
-      "https://cinebook.netlify.app/movie-details/:id",
+      "https://cinebook.netlify.app/movie-details/*",
     ],
   })
 );
